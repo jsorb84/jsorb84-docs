0c12757ba64dd617aee499ee0670f0279e2d64b5
jsorb84 <105475254+jsorb84@users.noreply.github.com>
2023-09-16 18:01:55 -0400

    Initial

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,18 +1,27 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import React from "react";
+import { DocsThemeConfig } from "nextra-theme-docs";
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
-  project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
-  },
-  chat: {
-    link: 'https://discord.com',
-  },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
-  footer: {
-    text: 'Nextra Docs Template',
-  },
-}
+	logo: (
+		<>
+			<img
+				width={45}
+				height={45}
+				src="https://avatars.githubusercontent.com/u/105475254?v=4"
+			/>
+			<span>Sorber™ Consulting</span>
+		</>
+	),
+	project: {
+		link: "https://github.com/jsorb84",
+	},
+	chat: {
+		link: "https://discord.gg/VnxWB5pZdH",
+	},
+	docsRepositoryBase: "https://github.com/jsorb84/jsorb84-docs",
+	footer: {
+		text: "J.R. Sorber - Sorber™ Enterprises 2023",
+	},
+};
 
-export default config
+export default config;
